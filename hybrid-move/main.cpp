2ff359dff48b9dd7e23f2a5636e53d4a8e48cb52
Will Saunders <will.saunders@ukaea.uk>
2022-08-03 11:50:53 +0100

    quality of life impovements to hybrid bench

--- hybrid-move/main.cpp
+++ hybrid-move/main.cpp
@@ -3,11 +3,12 @@
 #include <neso_particles.hpp>
 #include <chrono>
 #include <csignal>
+#include <string>
 
 using namespace cl;
 using namespace NESO::Particles;
 
-inline void global_move_driver(){
+inline void hybrid_move_driver(const int N_total){
   const int ndim = 2;
   std::vector<int> dims(ndim);
   dims[0] = 16;
@@ -40,8 +41,18 @@ inline void global_move_driver(){
   std::mt19937 rng_rank(18241);
 
   const int rank = sycl_target.comm_pair.rank_parent;
+  const int size = sycl_target.comm_pair.size_parent;
+  
+  
+  int rstart, rend;
+  get_decomp_1d(size, N_total, rank, &rstart, &rend);
+  const int N = rend - rstart;
 
-  const int N = 100000;
+  int N_check = -1;
+  MPICHK(MPI_Allreduce(&N, &N_check, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD));
+  NESOASSERT(N_check == N_total, "Error creating particles");
+
+
   const int Nsteps_warmup = 1024;
   const int Nsteps = 2048;
   const REAL dt = 0.001;
@@ -50,17 +61,15 @@ inline void global_move_driver(){
   if (rank == 0){
     std::cout << "Starting..." << std::endl;
   }
-
+  
+  if (N > 0){
   auto positions =
       uniform_within_extents(N, ndim, mesh.global_extents, rng_pos);
   auto velocities = NESO::Particles::normal_distribution(
       N, 3, 0.0, 0.5, rng_vel);
-
   std::uniform_int_distribution<int> uniform_dist(
       0, sycl_target.comm_pair.size_parent - 1);
-
   ParticleSet initial_distribution(N, A.get_particle_spec());
-
   for (int px = 0; px < N; px++) {
     for (int dimx = 0; dimx < ndim; dimx++) {
       initial_distribution[Sym<REAL>("P")][px][dimx] = positions[dimx][px];
@@ -74,12 +83,11 @@ inline void global_move_driver(){
     const auto px_rank = uniform_dist(rng_rank);
     initial_distribution[Sym<INT>("NESO_MPI_RANK")][px][0] = px_rank;
   }
-
-
-  if (sycl_target.comm_pair.rank_parent == 0) {
-    A.add_particles_local(initial_distribution);
+  A.add_particles_local(initial_distribution);
   }
   reset_mpi_ranks(A[Sym<INT>("NESO_MPI_RANK")]);
+
+
   if (rank == 0){
     std::cout << "Particles Added..." << std::endl;
   }
@@ -133,7 +141,7 @@ inline void global_move_driver(){
 
   MPI_Barrier(sycl_target.comm_pair.comm_parent);
   if (rank == 0){
-    std::cout << "Particles Distributed..." << std::endl;
+    std::cout << N_total << " Particles Distributed..." << std::endl;
   }
 
   for (int stepx = 0; stepx < Nsteps_warmup; stepx++) {
@@ -198,8 +206,15 @@ int main(int argc, char **argv) {
     std::cout << "ERROR: MPI_Init != MPI_SUCCESS" << std::endl;
     return -1;
   }
+ 
+  if (argc > 1){
+    
+    std::string argv0 = std::string(argv[1]);
+    const int N = std::stoi(argv0);
 
-  global_move_driver();
+    hybrid_move_driver(N);
+
+  }
 
   if (MPI_Finalize() != MPI_SUCCESS) {
     std::cout << "ERROR: MPI_Finalize != MPI_SUCCESS" << std::endl;
