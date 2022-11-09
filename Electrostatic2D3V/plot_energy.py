8eb1f6a7433c1dc5693d6f13590893c1f63f076e
Will Saunders <will.saunders@ukaea.uk>
2022-11-09 11:56:13 +0000

    energy plotting script update

--- Electrostatic2D3V/plot_energy.py
+++ Electrostatic2D3V/plot_energy.py
@@ -7,98 +7,169 @@ import matplotlib.ticker as ticker
 from mpl_toolkits.axes_grid1 import Divider, Size
 from mpl_toolkits.axes_grid1.mpl_axes import Axes
 import h5py
+import xml.etree.ElementTree as ET
 
-plt.rc('xtick', labelsize='small')
-plt.rc('ytick', labelsize='small')
-plt.rc('text', usetex=True)
-plt.rc('savefig', dpi=500)
-plt.rc('xtick', labelsize='small')
-plt.rc('legend', fontsize='medium')
-plt.rc('legend', edgecolor='lightgray')
-plt.rc('ytick', labelsize='small')
-plt.rcParams.update({'font.size': 20})
+plt.rc("xtick", labelsize="small")
+plt.rc("ytick", labelsize="small")
+plt.rc("text", usetex=True)
+plt.rc("savefig", dpi=500)
+plt.rc("xtick", labelsize="small")
+plt.rc("legend", fontsize="medium")
+plt.rc("legend", edgecolor="lightgray")
+plt.rc("ytick", labelsize="small")
+plt.rcParams.update({"font.size": 20})
 
-if 'poster' in sys.argv:
-    print("Using poster font")
-    import matplotlib.font_manager as mfm
-    print(mfm.findfont('Lato'))
-    plt.rc('font', family='sans-serif', serif='Lato')
-    params = {'text.latex.preamble': [r'''
-    \usepackage{amsmath}
-    \usepackage{cmbright}
-    \usepackage[default]{lato}
-    ''']}
-else:
-    plt.rc('font', family='serif', serif='Times New Roman')
-    params = {'text.latex.preamble': r"\usepackage{amsmath}"}
+plt.rc("font", family="serif", serif="Times New Roman")
+params = {"text.latex.preamble": r"\usepackage{amsmath}"}
 
 plt.rcParams.update(params)
 
 
-def format_exp(v):
-    return '{: 2.1e}'.format(v)
+if __name__ == "__main__":
 
-def prettyInt(x, latex=True):
-    if (latex):
-        s = ('%6.1e' % x)
-        abscissa, exponent = s.split('e')
-        return r'$'+abscissa+r'\cdot 10^{'+str(int(exponent))+r'}$'
-    else:
-        s = ('%4.1f' % (1.E-6*x))+'mio'
-        return s
+    if (len(sys.argv) < 5) or ("--help" in sys.argv) or ("-h" in sys.argv):
+        print(
+            """
+Plots energies from electrostatic PIC. Call with:
 
+python3 plot_energy.py input.xml field_energy.h5 potential_energy.h5 kinetic_energy.h5
 
-if __name__ == '__main__':
+"""
+        )
+        quit()
 
-    
-    energy_h5 = h5py.File(sys.argv[1], "r")
-    
-    keys = sorted(energy_h5.keys(), key=lambda x: int(x.split("#")[-1]))
+    xml_tree = ET.parse(sys.argv[1])
+    xml_root = xml_tree.getroot()
 
+    parameters = xml_root.find("CONDITIONS").find("PARAMETERS").findall("P")
 
+    dt = -1
+    for px in parameters:
+        text = [tx for tx in px.itertext()][0].strip()
+        if text.startswith("particle_time_step"):
+            dt = float(text.split("=")[-1])
+    assert dt > 0.0
+
+    field_energy = h5py.File(sys.argv[2], "r")
+    keys = sorted(field_energy.keys(), key=lambda x: int(x.split("#")[-1]))
     N = len(keys)
     x = np.zeros((N,))
     y = np.zeros((N,))
     for keyi, keyx in enumerate(keys):
-        # TODO write dt, step number
-        x[keyi] = 0.001 * keyi * 20
-        y[keyi] = energy_h5[keyx]["field_energy"][0]
+        x[keyi] = dt * int(keyx.split("#")[-1])
+        y[keyi] = field_energy[keyx]["field_energy"][0]
 
- 
+    potential_energy = h5py.File(sys.argv[3], "r")
+    potential_keys = sorted(
+        potential_energy.keys(), key=lambda x: int(x.split("#")[-1])
+    )
+    N_potential = len(potential_keys)
+    potential_x = np.zeros((N_potential,))
+    potential_y = np.zeros((N_potential,))
+    for keyi, keyx in enumerate(potential_keys):
+        potential_x[keyi] = dt * int(keyx.split("#")[-1])
+        potential_y[keyi] = 0.5 * potential_energy[keyx]["potential_energy"][0]
+
+    kinetic_energy = h5py.File(sys.argv[4], "r")
+    kinetic_keys = sorted(
+        kinetic_energy.keys(), key=lambda x: int(x.split("#")[-1])
+    )
+    N_kinetic = len(kinetic_keys)
+    kinetic_x = np.zeros((N_kinetic,))
+    kinetic_y = np.zeros((N_kinetic,))
+    for keyi, keyx in enumerate(kinetic_keys):
+        kinetic_x[keyi] = dt * int(keyx.split("#")[-1])
+        kinetic_y[keyi] = kinetic_energy[keyx]["kinetic_energy"][0]
+
+    assert kinetic_x.shape == potential_x.shape
+    total_x = kinetic_x
+    total_y = potential_y + kinetic_y
+
+    # plot field energy and potential energy
     fig = plt.figure(figsize=(8, 6))
     ax = fig.add_subplot(111)
-    
-    ax.plot(x, y, color='b', label='Energy', linewidth=2,markersize=8)
+    ax2 = ax.twinx()
 
-    #ax.set_xscale('log')
-    ax.set_yscale('log')
+    field_colour = "b"
+    potential_colour = "g"
+    kinetic_colour = "r"
+    total_colour = "k"
 
-    ax.set_xlabel(r'Time')
-    ax.set_ylabel(r'Field Energy: $\int_{\Omega} u^2 dx$')
+    ax.plot(
+        x,
+        y,
+        color=field_colour,
+        label="Field Energy",
+        linewidth=2,
+        markersize=8,
+    )
+    ax.plot(
+        potential_x,
+        potential_y,
+        color=potential_colour,
+        label="Potential Energy",
+        linewidth=2,
+        markersize=8,
+    )
 
-    #ax2 = ax.twiny()
-    #ax2.set_axes_locator(divider.new_locator(nx=1, ny=1))
-    #ax2.spines['right'].set_visible(False)
-    #ax2.spines['top'].set_visible(False)
-    #ax2.set_xscale('log')
-    #ax2.set_xlim(ax.get_xlim())
-    #ax2.set_xlabel('Charges per node',labelpad=20)
-    #ax2.set_xticks(xtick_locs)
-    #ax2.set_xticklabels(top_labels, rotation=30)
-    #ax2.tick_params(axis='x',which='minor', top=False)
+    ax.set_yscale("log")
+    ax2.set_yscale("log")
+    ax.set_xlabel(r"Time")
+    ax.set_ylabel(r"Field Energy: $\int_{\Omega} u^2 dx$")
+    ax2.set_ylabel(
+        r"Potential Energy: $\frac{1}{2}\sum_{i} \phi(\vec{r}_i)q_i$"
+    )
 
-    #ax.tick_params(axis='x', which='minor', bottom=False)
-    #ax.set_xticks(xtick_locs)
-    #ax.set_xticklabels(xtick_values, minor=False)
+    ax.yaxis.label.set_color(field_colour)
+    ax2.yaxis.label.set_color(potential_colour)
 
+    fig.savefig("field_energy.pdf")
 
-    fig.savefig('energy.pdf')#, bbox_inches='tight')
+    # plot potential, kinetic and total energy
+    fig = plt.figure(figsize=(10, 6))
+    te_ax = fig.add_subplot(111)
 
+    pe_ax = te_ax.twinx()
+    ke_ax = te_ax.twinx()
+    ke_ax.spines.right.set_position(("axes", 1.15))
 
+    pe_ax.plot(
+        potential_x,
+        potential_y,
+        color=potential_colour,
+        label="Potential Energy",
+        linewidth=2,
+        markersize=8,
+    )
+    ke_ax.plot(
+        kinetic_x,
+        kinetic_y,
+        color=kinetic_colour,
+        label="Kinetic Energy",
+        linewidth=2,
+        markersize=8,
+    )
+    te_ax.plot(
+        total_x,
+        total_y,
+        color=total_colour,
+        label="Total Energy",
+        linewidth=2,
+        markersize=8,
+    )
 
+    # ax.set_yscale('log')
+    te_ax.set_xlabel(r"Time")
+    pe_ax.set_ylabel(
+        r"Potential Energy: $\mathcal{U} = \frac{1}{2}\sum_{i} \phi(\vec{r}_i)q_i$",
+        color=potential_colour,
+    )
+    ke_ax.set_ylabel(
+        r"Kinetic Energy: $\mathcal{K} = \sum_{i} \frac{1}{2}m_i|\vec{v}_i|^2 $",
+        color=kinetic_colour,
+    )
+    te_ax.set_ylabel(
+        r"Total Energy: $ \mathcal{U} + \mathcal{K} $", color=total_colour
+    )
 
-
-
-
-
-
+    fig.savefig("all_energy.pdf", bbox_inches="tight")
