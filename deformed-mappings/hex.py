85bf5d67b1538a8fb5be1d8655f330380dda603d
Will Saunders <will.saunders@ukaea.uk>
2023-06-01 18:09:26 +0100

    hex newton mapping passing self tests

--- deformed-mappings/hex.py
+++ deformed-mappings/hex.py
@@ -128,10 +128,57 @@ if __name__ == "__main__":
         xi[0] = xin[0]
         xi[1] = xin[1]
         xi[2] = xin[2]
-        print(stepx, residual)
 
     assert abs(xi[0] - xi_correct[0]) < 1.0e-14
     assert abs(xi[1] - xi_correct[1]) < 1.0e-14
+
+    vertices_test = (
+        (-1, -1, -1),
+        (-0.8, -1, -1),
+        (-0.8, -0.8, -1),
+        (-1, -0.8, -1),
+        (-1, -1, -0.8),
+        (-0.8, -1, -0.8),
+        (-0.8, -0.8, -0.8),
+        (-1, -0.8, -0.8),
+    )
+    hex_test = LinearGeomEvaluate(hex_x, vertices_test)
+    hex_newton = Newton(hex_x)
+
+    hex_newton_evaluate = NewtonEvaluate(hex_newton, hex_test)
+
+    for vi, vx in enumerate(vertices_ref):
+        to_test = hex_test.x(vx)
+        correct = vertices_test[vi]
+        assert np.linalg.norm(np.array(correct).ravel() - np.array(to_test).ravel(), np.inf) < 1.0e-15
+        to_test = hex_test.f(vx, correct)
+        assert np.linalg.norm(np.array(to_test).ravel(), np.inf) < 1.0e-15
+
+    xi_correct0 = -0.6
+    xi_correct1 = -0.7
+    xi_correct2 = -0.9
+
+    xi_correct = (xi_correct0, xi_correct1, xi_correct2)
+    phys = hex_test.x(xi_correct)
+    residual, fv = hex_newton_evaluate.residual(xi_correct, phys)
+    assert residual < 1.0e-15
+    phys_nektar = (-0.960000, -0.970000, -0.990000)
+
+    assert abs(phys_nektar[0] - phys[0]) < 1.0e-8
+    assert abs(phys_nektar[1] - phys[1]) < 1.0e-8
+    assert abs(phys_nektar[2] - phys[2]) < 1.0e-8
+
+    xi = [0.0, 0.0, 0.0]
+    for stepx in range(5):
+        residual, fv = hex_newton_evaluate.residual(xi, phys)
+        xin = hex_newton_evaluate.step(xi, phys, fv)
+        xi[0] = xin[0]
+        xi[1] = xin[1]
+        xi[2] = xin[2]
+
+    assert abs(xi[0] - xi_correct[0]) < 1.0e-14
+    assert abs(xi[1] - xi_correct[1]) < 1.0e-14
+    assert abs(xi[2] - xi_correct[2]) < 1.0e-14
 
     hex_newton_ccode = NewtonLinearCCode(hex_newton)
     print(hex_newton_ccode.residual())
