2e24791cd937d3a0c66cb7cc4f81c03146bf84e8
Will Saunders <will.saunders@ukaea.uk>
2023-06-01 15:20:22 +0100

    refactored sympy code to generate newton implementations

--- deformed-mappings/quad.py
+++ deformed-mappings/quad.py
@@ -4,327 +4,379 @@ import numpy as np
 init_printing(use_unicode=True)
 
 
-xi0 = symbols("xi0")
-xi1 = symbols("xi1")
-# xi2 = symbols("xi2")
-xi = Matrix([xi0, xi1])
+def make_vector(*args):
+    return Matrix([symbols(ax) for ax in args])
 
-v00 = symbols("v00")
-v01 = symbols("v01")
-v10 = symbols("v10")
-v11 = symbols("v11")
-v20 = symbols("v20")
-v21 = symbols("v21")
-v30 = symbols("v30")
-v31 = symbols("v31")
-
-xin0 = symbols("xin0")
-xin1 = symbols("xin1")
-xin = Matrix([xin0, xin1])
-
-phys0 = symbols("phys0")
-phys1 = symbols("phys1")
-phys = Matrix(
-    [
-        phys0,
-        phys1
-    ]
-)
-
-fv0 = symbols("f0")
-fv1 = symbols("f1")
-fv = Matrix(
-    [
-        fv0,
-        fv1
-    ]
-)
-
-def get_x():
-    x = Matrix(
-        [
-            0.25 * v00 * (1 - xi0) * (1 - xi1) + \
-            0.25 * v10 * (1 + xi0) * (1 - xi1) + \
-            0.25 * v30 * (1 - xi0) * (1 + xi1) + \
-            0.25 * v20 * (1 + xi0) * (1 + xi1),
-            0.25 * v01 * (1 - xi0) * (1 - xi1) + \
-            0.25 * v11 * (1 + xi0) * (1 - xi1) + \
-            0.25 * v31 * (1 - xi0) * (1 + xi1) + \
-            0.25 * v21 * (1 + xi0) * (1 + xi1)
-        ]
-    )
-    return x
-
-x = get_x()
-f = x - phys
-J = f.jacobian(xi)
-
-def newton_step(vertices, a0, a1, phys0_val, phys1_val):
-
-    subs = {
-        xi0 :  a0,
-        xi1 :  a1,
-        phys0: phys0_val,
-        phys1: phys1_val,
-    }
-    subs.update(vertices)
-
-    xin0v = step_list[0].evalf(subs=subs)
-    xin1v = step_list[1].evalf(subs=subs)
-    return xin0v, xin1v
-
-def xi_to_phys(vertices, xi0v, xi1v):
-
-    subs = {
-        xi0 :  xi0v,
-        xi1 :  xi1v
-    }
-    subs.update(vertices)
-
-    physv = x.evalf(subs=subs)
-    return physv
+class SymbolicCommon:
+    def __init__(self, geom):
+        self.geom = geom
+        self.ndim = geom.ndim
+        self.xi = make_vector(*["xi{}".format(dx) for dx in range(self.ndim)])
+        self.phys = make_vector(*["phys{}".format(dx) for dx in range(self.ndim)])
+        self._x = self.geom.get_x(self.xi)
+        self._f = self.geom.get_f(self.xi, self.phys)
 
 
-def newton_residual(vertices, xi0, xi1, phys0v, phys1v):
-    target = np.array((phys0v, phys1v)).ravel()
-    out = np.array(xi_to_phys(vertices, xi0, xi1)).ravel()
+class NewtonCommon(SymbolicCommon):
+    def __init__(self, geom):
+        SymbolicCommon.__init__(self, geom)
+        self.xi_next = make_vector(*["xin{}".format(dx) for dx in range(self.ndim)])
+        self.fv = make_vector(*["f{}".format(dx) for dx in range(self.ndim)])
 
-    return np.linalg.norm(target - out, np.inf)
+class LinearEvaluateCommon:
+    def __init__(self, geom, vertices):
+        assert len(vertices) == geom.num_vertices
+        self.vertices = vertices
+        self.sub_vertices = {}
+        for vx in range(geom.num_vertices):
+            for dimx in range(self.ndim):
+                self.sub_vertices[geom.vertices[vx][dimx]] = self.vertices[vx][dimx]
 
 
+class Newton(NewtonCommon):
+    def __init__(self, geom):
+        NewtonCommon.__init__(self, geom)
+        self.J = self._f.jacobian(self.xi)
+        self.step = solve(self.xi_next - self.xi + (self.J**(-1)) * self.fv, self.xi_next, dict=True)
+        self.step_components = [self.step[0][self.xi_next[dimx]] for dimx in range(self.ndim)]
+        self.f = self._f
+        self.x = self._x
 
-def print_x(step):
-    expand_opt = create_expand_pow_optimization(99)
-    print(
+
+class NewtonEvaluate:
+    def __init__(self, newton, evaluate):
+        self.newton = newton
+        self.evaluate = evaluate
+        assert self.newton.geom == evaluate.geom
+
+    def residual(self, xi, phys):
+        subs = {}
+        subs.update(self.evaluate.sub_vertices)
+        for dimx in range(self.evaluate.geom.ndim):
+            subs[self.evaluate.phys[dimx]] = phys[dimx]
+            subs[self.evaluate.xi[dimx]] = xi[dimx]
+
+        e = self.newton.f.evalf(subs=subs)
+        efloat = [float(ex) for ex in e]
+
+        r = 0.0
+        for ex in efloat:
+            r = max(r, abs(ex))
+        return r, efloat
+
+
+    def step(self, xi, phys, fv):
+        subs = {}
+        subs.update(self.evaluate.sub_vertices)
+        ndim = self.evaluate.geom.ndim
+        for dimx in range(ndim):
+            subs[self.evaluate.phys[dimx]] = phys[dimx]
+            subs[self.evaluate.xi[dimx]] = xi[dimx]
+            subs[self.newton.fv[dimx]] = fv[dimx]
+
+        xin = [self.newton.step_components[dimx].evalf(subs=subs) for dimx in range(ndim)]
+        xinfloat = [float(ex) for ex in xin]
+        return xinfloat
+
+
+class NewtonLinearCCode:
+    def __init__(self, newton):
+        self.newton = newton
+        
+
+    def residual(self):
+        ndim = self.newton.geom.ndim
+        expand_opt = create_expand_pow_optimization(99)
+
+
+        component_name = ("x", "y", "z")
+        docs_params = []
+        args = []
+        for dimx in range(ndim):
+            n = f"xi{dimx}"
+            args.append(f"const REAL {n}")
+            docs_params.append(f"@param[in] {n} Current xi_n point, {component_name[dimx]} component.")
+
+        for vi, vx in enumerate(self.newton.geom.vertices):
+            for dimx in range(ndim):
+                n = f"v{vi}{dimx}"
+                args.append(
+                    f"const REAL {n}"
+                )
+                docs_params.append(f"@param[in] {n} Vertex {vi}, {component_name[dimx]} component.")
+
+        for dimx in range(ndim):
+            n = f"phys{dimx}"
+            args.append(f"const REAL {n}")
+            docs_params.append(f"@param[in] {n} Target point in global space, {component_name[dimx]} component.")
+
+        for dimx in range(ndim):
+            n = f"f{dimx}"
+            args.append(f"REAL * {n}")
+            docs_params.append(f"@param[in, out] {n} Current f evaluation at xi, {component_name[dimx]} component.")
+
+        params = "\n * ".join(docs_params)
+        x_description = "\n * ".join(self.newton.geom.x_description.split("\n"))
+        
+        docstring = f"""
+/**
+ * Compute and return F evaluation where
+ * 
+ * F(xi) = X(xi) - X_phys
+ * 
+ * where X_phys are the global coordinates. X is defined as
+ * 
+ * {x_description}
+ *
+ * This is a generated function. To modify this function please edit the script
+ * that generates this function.
+ *
+ * {params}
+ */
 """
+        
+        args_string = ",\n".join(args)
+        name = self.newton.geom.name
+        s = f"""inline void newton_f_{name}(
+{args_string}
+)"""
+        
+        instr = ["{"]
+        steps = [fx for fx in self.newton.f]
+        cse_list = cse(steps, optimizations='basic')
+        for cse_expr in cse_list[0]:
+            lhs = cse_expr[0]
+            rhs = expand_opt(cse_expr[1])
+            expr = f"const REAL {lhs} = {rhs};"
+            instr.append(expr)
+
+        for dimx in range(ndim):
+            instr.append(
+                f"const REAL f{dimx}_tmp = {cse_list[1][dimx]};"
+            )
+        for dimx in range(ndim):
+            instr.append(
+                f"*f{dimx} = f{dimx}_tmp;"
+            )
+
+        s+="\n  ".join(instr)
+        s += "\n}\n"
+
+        return docstring + s
+
+
+    def step(self):
+        ndim = self.newton.geom.ndim
+        expand_opt = create_expand_pow_optimization(99)
+
+
+        component_name = ("x", "y", "z")
+        docs_params = []
+        args = []
+        for dimx in range(ndim):
+            n = f"xi{dimx}"
+            args.append(f"const REAL {n}")
+            docs_params.append(f"@param[in] {n} Current xi_n point, {component_name[dimx]} component.")
+
+        for vi, vx in enumerate(self.newton.geom.vertices):
+            for dimx in range(ndim):
+                n = f"v{vi}{dimx}"
+                args.append(
+                    f"const REAL {n}"
+                )
+                docs_params.append(f"@param[in] {n} Vertex {vi}, {component_name[dimx]} component.")
+
+        for dimx in range(ndim):
+            n = f"phys{dimx}"
+            args.append(f"const REAL {n}")
+            docs_params.append(f"@param[in] {n} Target point in global space, {component_name[dimx]} component.")
+
+        for dimx in range(ndim):
+            n = f"f{dimx}"
+            args.append(f"const REAL {n}")
+            docs_params.append(f"@param[in] {n} Current f evaluation at xi, {component_name[dimx]} component.")
+
+        for dimx in range(ndim):
+            n = f"xin{dimx}"
+            args.append(f"REAL * {n}")
+            docs_params.append(f"@param[in, out] {n} Output local coordinate iteration, {component_name[dimx]} component.")
+
+        params = "\n * ".join(docs_params)
+        x_description = "\n * ".join(self.newton.geom.x_description.split("\n"))
+        
+        docstring = f"""
 /**
  * Perform a Newton method update step for a Newton iteration that determines
  * the local coordinates (xi) for a given set of physical coordinates. If
  * v0,v1,v2 and v3 (passed component wise) are the vertices of a linear sided
  * quadrilateral then this function performs the Newton update:
  * 
- * xi_{n+1} = xi_n - J^{-1}(xi_n) * F(xi_n)
+ * xi_{{n+1}} = xi_n - J^{{-1}}(xi_n) * F(xi_n)
  * 
  * where
  * 
  * F(xi) = X(xi) - X_phys
  * 
- * where X_phys are the global coordinates.
+ * where X_phys are the global coordinates. 
  * 
+ * X is defined as
+ * 
+ * {x_description}
+ *
  * This is a generated function. To modify this function please edit the script
  * that generates this function.
- * 
- * @param[in] xi0 Current xi_n point, x component.
- * @param[in] xi1 Current xi_n point, y component.
- * @param[in] v00 Vertex 0, x component of quadrilateral.
- * @param[in] v01 Vertex 0, y component of quadrilateral.
- * @param[in] v10 Vertex 1, x component of quadrilateral.
- * @param[in] v11 Vertex 1, y component of quadrilateral.
- * @param[in] v20 Vertex 2, x component of quadrilateral.
- * @param[in] v21 Vertex 2, y component of quadrilateral.
- * @param[in] v30 Vertex 3, x component of quadrilateral.
- * @param[in] v31 Vertex 3, y component of quadrilateral.
- * @param[in] phys0 Target point in physical space, x component.
- * @param[in] phys1 Target point in physical space, y component.
- * @param[in] f0 Current f evaluation at xi, x component.
- * @param[in] f1 Current f evaluation at xi, y component.
- * @param[in, out] xin0 Output local coordinate iteration, x component.
- * @param[in, out] xin1 Output local coordinate iteration, y component.
+ *
+ * {params}
  */
-inline void newton_step_linear_2d(
-const REAL xi0,
-const REAL xi1,
-const REAL v00,
-const REAL v01,
-const REAL v10,
-const REAL v11,
-const REAL v20,
-const REAL v21,
-const REAL v30,
-const REAL v31,
-const REAL phys0,
-const REAL phys1,
-const REAL f0,
-const REAL f1,
-REAL * xin0,
-REAL * xin1
-){"""
-    )
-    step_list = [step[0][xin0], step[0][xin1]]
-    cse_list = cse(step_list, optimizations='basic')
-    for cse_expr in cse_list[0]:
-        lhs = cse_expr[0]
-        rhs = expand_opt(cse_expr[1])
-        expr = f"const REAL {lhs} = {rhs};"
-        print(expr)
-
-    expr_xin0 = f"const REAL xin0_tmp = {cse_list[1][0]};"
-    expr_xin1 = f"const REAL xin1_tmp = {cse_list[1][1]};"
-    print(expr_xin0)
-    print(expr_xin1)
-    print("*xin0 = xin0_tmp;")
-    print("*xin1 = xin1_tmp;")
-    print("}\n")
-
-    print(
 """
-/**
- * Compute and return F evaluation for Quadrilateral where
- * 
- * F(xi) = X(xi) - X_phys
- * 
- * where X_phys are the global coordinates.
- * 
- * This is a generated function. To modify this function please edit the script
- * that generates this function.
- * 
- * @param[in] xi0 Current xi_n point, x component.
- * @param[in] xi1 Current xi_n point, y component.
- * @param[in] v00 Vertex 0, x component of quadrilateral.
- * @param[in] v01 Vertex 0, y component of quadrilateral.
- * @param[in] v10 Vertex 1, x component of quadrilateral.
- * @param[in] v11 Vertex 1, y component of quadrilateral.
- * @param[in] v20 Vertex 2, x component of quadrilateral.
- * @param[in] v21 Vertex 2, y component of quadrilateral.
- * @param[in] v30 Vertex 3, x component of quadrilateral.
- * @param[in] v31 Vertex 3, y component of quadrilateral.
- * @param[in] phys0 Target point in physical space, x component.
- * @param[in] phys1 Target point in physical space, y component.
- * @param[in, out] f0 Current f evaluation at xi, x component.
- * @param[in, out] f1 Current f evaluation at xi, y component.
- */
-inline void newton_f_linear_2d(
-const REAL xi0,
-const REAL xi1,
-const REAL v00,
-const REAL v01,
-const REAL v10,
-const REAL v11,
-const REAL v20,
-const REAL v21,
-const REAL v30,
-const REAL v31,
-const REAL phys0,
-const REAL phys1,
-REAL * f0,
-REAL * f1
-){"""
-    )
-    step_list = [f[0], f[1]]
-    cse_list = cse(step_list, optimizations='basic')
-    for cse_expr in cse_list[0]:
-        lhs = cse_expr[0]
-        rhs = expand_opt(cse_expr[1])
-        expr = f"const REAL {lhs} = {rhs};"
-        print(expr)
+        
+        args_string = ",\n".join(args)
+        name = self.newton.geom.name
+        s = f"""inline void newton_step_{name}(
+{args_string}
+)"""
+        
+        instr = ["{"]
+        cse_list = cse(self.newton.step_components, optimizations='basic')
+        for cse_expr in cse_list[0]:
+            lhs = cse_expr[0]
+            rhs = expand_opt(cse_expr[1])
+            expr = f"const REAL {lhs} = {rhs};"
+            instr.append(expr)
 
-    expr_xin0 = f"const REAL f0_tmp = {cse_list[1][0]};"
-    expr_xin1 = f"const REAL f1_tmp = {cse_list[1][1]};"
-    print(expr_xin0)
-    print(expr_xin1)
-    print("*f0 = f0_tmp;")
-    print("*f1 = f1_tmp;")
-    print("}\n")
+        for dimx in range(ndim):
+            instr.append(
+                f"const REAL xin{dimx}_tmp = {cse_list[1][dimx]};"
+            )
+        for dimx in range(ndim):
+            instr.append(
+                f"*xin{dimx} = xin{dimx}_tmp;"
+            )
+        s+="\n  ".join(instr)
+        s += "\n}\n"
 
+        return docstring + s
+
+
+
+class LinearGeomEvaluate(SymbolicCommon):
+    def __init__(self, geom, vertices):
+        SymbolicCommon.__init__(self, geom)
+        LinearEvaluateCommon.__init__(self, geom, vertices)
+
+
+    def x(self, xi):
+        subs = {}
+        for dimx in range(self.ndim):
+            subs[self.xi[dimx]] = xi[dimx]
+        subs.update(self.sub_vertices)
+        return [float(fx) for fx in self._x.evalf(subs=subs)]
+
+
+    def f(self, xi, phys):
+        subs = {}
+        for dimx in range(self.ndim):
+            subs[self.xi[dimx]] = xi[dimx]
+            subs[self.phys[dimx]] = phys[dimx]
+        subs.update(self.sub_vertices)
+        return [float(fx) for fx in self._f.evalf(subs=subs)]
+
+
+class LinearQuad:
+    def __init__(self):
+
+        self.num_vertices = 4
+        self.ndim = 2
+        self.vertices = [make_vector("v{}0".format(vx), "v{}1".format(vx)) for vx in range(self.num_vertices)]
+        self.name = "linear_2d"
+        self.x_description = """
+X(xi) = 0.25 * v0 * (1 - xi_0) * (1 - xi_1) + 
+        0.25 * v1 * (1 + xi_0) * (1 - xi_1) + 
+        0.25 * v3 * (1 - xi_0) * (1 + xi_1) + 
+        0.25 * v2 * (1 + xi_0) * (1 + xi_1)
+"""
+
+    def get_x(self, xi):
+        
+        v = self.vertices
+        x = Matrix(
+            [
+                0.25 * v[0][0] * (1 - xi[0]) * (1 - xi[1]) + \
+                0.25 * v[1][0] * (1 + xi[0]) * (1 - xi[1]) + \
+                0.25 * v[3][0] * (1 - xi[0]) * (1 + xi[1]) + \
+                0.25 * v[2][0] * (1 + xi[0]) * (1 + xi[1]),
+                0.25 * v[0][1] * (1 - xi[0]) * (1 - xi[1]) + \
+                0.25 * v[1][1] * (1 + xi[0]) * (1 - xi[1]) + \
+                0.25 * v[3][1] * (1 - xi[0]) * (1 + xi[1]) + \
+                0.25 * v[2][1] * (1 + xi[0]) * (1 + xi[1])
+            ]
+        )
+        return x
+
+    def get_f(self, xi, phys):
+        x = self.get_x(xi)
+        f = x - phys
+        return f
 
 
 
 
 if __name__ == "__main__":
 
-
-
-    step = solve(xin - xi + (J**(-1)) * fv, xin, dict=True)
-    step_list = [step[0][xin0], step[0][xin1]]
-
-    vertices_ref = {
-        v00 : -1.0,
-        v01 : -1.0,
-        v10 :  1.0,
-        v11 : -1.0,
-        v20 :  1.0,
-        v21 :  1.0,
-        v30 : -1.0,
-        v31 :  1.0,
-    }
-
-    test_vertices = (
+    quad = LinearQuad()
+    
+    vertices_ref = (
         (-1.0, -1.0),
         ( 1.0, -1.0),
-        (-1.0,  1.0),
-        ( 1.0,  1.0),
+        ( 1.0, 1.0),
+        (-1.0, 1.0),
     )
-    for vx in test_vertices:
-        correct = np.array(vx).ravel()
-        to_test = np.array(xi_to_phys(vertices_ref, vx[0], vx[1])).ravel()
-        abs_error = np.linalg.norm(correct - to_test, np.inf)
-        assert abs_error < 1.0E-14, f"self test failed at vertex {vx}, error {abs_error}"
+    quad_ref = LinearGeomEvaluate(quad, vertices_ref)
     
-
-    vertices = {
-        v00 : -3.0,
-        v01 : -2.0,
-        v10 :  1.0,
-        v11 : -1.0,
-        v20 :  2.0,
-        v21 :  2.0,
-        v30 : -1.0,
-        v31 :  4.0,
-        fv0 : 0.0,
-        fv1 : 0.0,
-    }
     
-
+    
+    for vx in vertices_ref:
+        to_test = quad_ref.x(vx)
+        correct = vx
+        assert np.linalg.norm(np.array(correct).ravel() - np.array(to_test).ravel(), np.inf) < 1.0E-15
+        to_test = quad_ref.f(vx, vx)
+        assert np.linalg.norm(np.array(to_test).ravel(), np.inf) < 1.0E-15
+    
+    
+    vertices_test = (
+        (-3.0, -2.0),
+        ( 1.0, -1.0),
+        ( 2.0, 2.0),
+        (-1.0, 4.0),
+    )
+    quad_test = LinearGeomEvaluate(quad, vertices_test)
+    quad_newton = Newton(quad)
+    
+    quad_newton_ccode = NewtonLinearCCode(quad_newton)
+    
+    print(quad_newton_ccode.residual())
+    print(quad_newton_ccode.step())
+    
+    
+    quad_newton_evaluate = NewtonEvaluate(quad_newton, quad_test)
+    
+    
     xi_correct0 = -0.9
     xi_correct1 = 0.8
-    test_phys = xi_to_phys(vertices, xi_correct0, xi_correct1)
-    test_phys0 = float(test_phys[0])
-    test_phys1 = float(test_phys[1])
-
-    xin0v = 0.0
-    xin1v = 0.0
-    fv0v = 0.0
-    fv1v = 0.0
-
-    res = newton_residual(vertices, xin0v, xin1v, test_phys0, test_phys1)
+    xi_correct = (xi_correct0, xi_correct1)
+    phys = quad_test.x(xi_correct)
+    residual, fv = quad_newton_evaluate.residual(xi_correct, phys)
+    assert residual < 1.0E-15
     
-    subs = {
-        phys0 : test_phys0,
-        phys1 : test_phys1,
-        xi0 : xin0v,
-        xi1 : xin1v,
-    }
-    subs.update(vertices)
-    tmp = f.evalf(subs=subs)
-    fv0v = float(tmp[0])
-    fv1v = float(tmp[1])
-    vertices[fv0] = fv0v
-    vertices[fv1] = fv1v
-
+    
+    xi = [0.0, 0.0]
     for stepx in range(5):
-        xin0v, xin1v = newton_step(vertices, xin0v, xin1v, test_phys0, test_phys1)
-        subs = {
-            phys0 : test_phys0,
-            phys1 : test_phys1,
-            xi0 : xin0v,
-            xi1 : xin1v,
-        }
-        subs.update(vertices)
-        tmp = f.evalf(subs=subs)
-        fv0v = float(tmp[0])
-        fv1v = float(tmp[1])
-        vertices[fv0] = fv0v
-        vertices[fv1] = fv1v
-        res = newton_residual(vertices, xin0v, xin1v, test_phys0, test_phys1)
-
-    assert abs(xi_correct0 - xin0v) < 1.0e-15, "self newton test failed"
-    assert abs(xi_correct1 - xin1v) < 1.0e-15, "self newton test failed"
-
-    print_x(step)
-
-
-
-
-
-
-
+        residual, fv = quad_newton_evaluate.residual(xi, phys)
+        xin = quad_newton_evaluate.step(xi, phys, fv)
+        xi[0] = xin[0]
+        xi[1] = xin[1]
+    
+    assert abs(xi[0] - xi_correct[0]) < 1.0E-14
+    assert abs(xi[1] - xi_correct[1]) < 1.0E-14
+    
+    
